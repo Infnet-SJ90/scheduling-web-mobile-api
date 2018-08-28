17df7271e40ee29458fdc68f5933ca79f2d2fc12
yurisouza <yuri.souza@al.infnet.edu.br>
2018-08-27 21:55:45 -0300

    :spakles: Atualiza injeção de dependência

--- src/SchedulingWebMobileApi.IoC/StartupIoC.cs
+++ src/SchedulingWebMobileApi.IoC/StartupIoC.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MySql.Data.MySqlClient;
 using SchedulingWebMobileApi.Application.AppServices;
 using SchedulingWebMobileApi.Application.Interfaces;
 using SchedulingWebMobileApi.Core.Interfaces;
 using SchedulingWebMobileApi.Core.Interfaces.Services;
+using SchedulingWebMobileApi.Core.Mapper;
 using SchedulingWebMobileApi.Core.Repository;
 using SchedulingWebMobileApi.Core.Services;
 using System;
@@ -16,23 +18,34 @@ namespace SchedulingWebMobileApi.IoC
     {
         public static void RegisterIoC(IServiceCollection services, IConfiguration configuration)
         {
+            services.AddSingleton<IMapperAdapter, MapperAdapter>();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
             RegisterRepository(services, configuration);
+            RegisterApplicationServices(services);
+            RegisterDomainServices(services);
         }
 
         private static void RegisterRepository(IServiceCollection services, IConfiguration configuration)
         {
             services.AddSingleton<IDbConnection>(new MySqlConnection(configuration.GetConnectionString("Database")));
             services.AddTransient<ICidadaoRepository, CidadaoRepository>();
+            services.AddTransient<IAuthRepository, AuthRepository>();
+            services.AddTransient<IAgendamentoRepository, AgendamentoRepository>();
         }
 
         private static void RegisterApplicationServices(IServiceCollection services)
         {
             services.AddTransient<ICidadaoAppService, CidadaoAppService>();
+            services.AddTransient<IAuthAppService, AuthAppService>();
+            services.AddTransient<IAgendamentoAppService, AgendamentoAppService>();
         }
 
         private static void RegisterDomainServices(IServiceCollection services)
         {
             services.AddTransient<ICidadaoService, CidadaoService>();
+            services.AddTransient<IAuthService, AuthService>();
+            services.AddTransient<IAgendamentoService, AgendamentoService>();
         }
     }
 }
