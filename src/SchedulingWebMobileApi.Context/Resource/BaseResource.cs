e32575601675d52142a1514d2197c867aee2d610
yurisouza <yuri.souza@al.infnet.edu.br>
2018-08-30 08:42:59 -0300

    :sparkles: Implementa context

---
+++ src/SchedulingWebMobileApi.Context/Resource/BaseResource.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SchedulingWebMobileApi.Context
+{
+    public abstract class BaseResource
+    {
+        private IHttpContextAccessor _httpContextAccessor = null;
+
+        public BaseResource(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            Context = new Context();
+        }
+
+        private Context _context;
+        public Context Context
+        {
+            get
+            {
+                if(this._context == null)
+                {
+                    this._context = (Context) _httpContextAccessor.HttpContext.Items["Context"];
+                }
+                return this._context;
+            }
+            set
+            {
+                value.Request = _httpContextAccessor.HttpContext.Request;
+                _httpContextAccessor.HttpContext.Items["Context"] = value;
+            }
+        }
+    }
+}
---
+++ src/SchedulingWebMobileApi.Context/Resource/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using SchedulingWebMobileApi.Domain;
+
+namespace SchedulingWebMobileApi.Context
+{
+    public class Context
+    {
+        public HttpRequest Request { get; set; }
+        public Cidadao Cidadao { get; set; }
+        //public Endereco Cidadao { get; set; }
+
+        public Context(){ }
+
+        public Context(Cidadao cidadao)
+        {
+            Cidadao = cidadao;
+        }
+    }
+
+}
