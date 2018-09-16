b5abe93b487cb61fe0303e615fc34d47bfc9bbb5
yurisouza <yuri.souza@al.infnet.edu.br>
2018-09-16 13:42:10 -0300

    :bug: Corrige formato de data na criação de uma scheduling

--- src/SchedulingWebMobileApi.Models/Models/Request/AgendamentoRequestModel.cs
+++ src/SchedulingWebMobileApi.Models/Models/Request/AgendamentoRequestModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using SchedulingWebMobileApi.Models.Utility;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
@@ -8,7 +10,8 @@ namespace SchedulingWebMobileApi.Models.Models.Request
 public class AgendamentoRequestModel
     {
         public Guid AgendamentoKey { get; set; }
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        [JsonConverter(typeof(JsonDateConverter))]
         public DateTime Data { get; set; }
         [DisplayFormat(DataFormatString = "{0:HH:m:s}", ApplyFormatInEditMode = true)]
         public DateTime Hora { get; set; }
---
+++ src/SchedulingWebMobileApi.Models/Utility/JsonDateConverter.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchedulingWebMobileApi.Models.Utility
+{
+    public class JsonDateConverter : IsoDateTimeConverter
+    {
+        public JsonDateConverter()
+        {
+            DateTimeFormat = "dd/MM/yyyy";
+        }
+    }
+}
