6b0698ad3b7568e450db4e3595f91142108b5187
yurisouza <yuri.souza@al.infnet.edu.br>
2018-09-17 09:18:16 -0300

    :sparkles: Insere status page api

---
+++ src/SchedulingWebMobileApi/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SchedulingWebMobileApi.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            var content = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/View/index.html");
+            content += _configuration.GetConnectionString("Database");
+            return new ContentResult()
+            {
+                Content = content,
+                ContentType = "text/plain",
+            };
+        }
+    }
+}
